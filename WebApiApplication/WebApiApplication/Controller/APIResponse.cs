368fc2c0654257effd659283e0b1b1a6d63e6717
nksmunj <nksmunj@gmail.com>
2018-12-14 11:35:47 +0530

    14 Dec changes

---
+++ WebApiApplication/WebApiApplication/Controller/APIResponse.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppleDiagnostics.Entity
+{
+    public class APIResponseEntity
+    {
+        public int success { get; set; }
+        public object data { get; set; }
+        public string message { get; set; }
+    }
+
+    public class APIResponse
+    {
+        public static APIResponseEntity Success(object data)
+        {
+            APIResponseEntity entity = new APIResponseEntity();
+            try
+            {
+                entity.success = 1;
+                entity.data = data;
+                entity.message = "Success";
+            }
+            catch (Exception ex) { throw ex; }
+            return entity;
+        }
+
+        public static APIResponseEntity Success()
+        {
+            APIResponseEntity entity = new APIResponseEntity();
+            try
+            {
+                entity.success = 1;
+                entity.data = string.Empty;
+                entity.message = "Success";
+            }
+            catch (Exception ex) { throw ex; }
+            return entity;
+        }
+
+        public static APIResponseEntity BadRequest(string message)
+        {
+            APIResponseEntity entity = new APIResponseEntity();
+            try
+            {
+                List<string> empty = new List<string>();
+                entity.success = 0;
+                entity.data = empty;
+                entity.message = message;
+            }
+            catch (Exception ex) { throw ex; }
+            return entity;
+        }
+    }
+}
---
+++ WebApiApplication/WebApiApplication/Controller/HelloController.cs
@@ -0,0 +1,65 @@
+﻿using AppleDiagnostics.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApiApplication.Controller
+{
+    public class HelloController : ApiController
+    {
+        //public HttpResponseMessage Get()
+        //{
+        //    return Request.CreateResponse(HttpStatusCode.OK, APIResponse.Success("Success Return"));
+        //}
+
+        // GET: api/student
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/student/5
+        public string Get(int id,string name,string age)
+        {
+            return "value";
+        }
+
+        // POST: api/student
+        public void Post(APIResponseEntity entity)
+        {
+        }
+
+        //// PUT: api/student/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+
+        // PUT: api/student/5
+        public void Put(int id, APIResponseEntity entity)
+        {
+        }
+
+        // DELETE: api/student/5
+        public void Delete(int id)
+        {
+        }
+
+        //Given above are the specific http verds other than these you may use other controllers too
+        [HttpGet]
+        public HttpResponseMessage GetPatientDetails()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, APIResponse.Success("Success Return"));
+        }
+
+        [HttpGet]
+        public string HelloMyNameIsNikhil()
+        {
+            return "Hello 123";
+        }
+    }
+}
